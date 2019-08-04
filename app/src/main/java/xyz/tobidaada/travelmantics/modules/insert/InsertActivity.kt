3b9a4f27a5d0641ea7af8bc7efaa3f4333ecb004
tobialbert <daadatobi@gmail.com>
2019-08-04 21:43:10 +0100

    Feat: Add InsertActivity.

---
+++ app/src/main/java/xyz/tobidaada/travelmantics/modules/insert/InsertActivity.kt
@@ -0,0 +1,13 @@
+package xyz.tobidaada.travelmantics.modules.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.tobidaada.travelmantics.R
+
+class InsertActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert)
+    }
+}
